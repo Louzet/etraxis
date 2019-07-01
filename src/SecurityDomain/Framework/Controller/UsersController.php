9ec8f2baa61b0ae5e4fd2a31448ad7fa9301dec0
Artem Rodygin <artem.rodygin@gmail.com>
2019-07-01 17:06:38 +1200

    #20 Admin / Set user’s password

--- src/SecurityDomain/Framework/Controller/UsersController.php
+++ src/SecurityDomain/Framework/Controller/UsersController.php
@@ -70,11 +70,12 @@ class UsersController extends AbstractController
             'themes'    => Theme::all(),
             'timezones' => Timezone::all(),
             'can'       => [
-                'update'  => $this->isGranted(UserVoter::UPDATE_USER, $entity),
-                'delete'  => $this->isGranted(UserVoter::DELETE_USER, $entity),
-                'disable' => $this->isGranted(UserVoter::DISABLE_USER, $entity),
-                'enable'  => $this->isGranted(UserVoter::ENABLE_USER, $entity),
-                'unlock'  => $this->isGranted(UserVoter::UNLOCK_USER, $entity),
+                'update'   => $this->isGranted(UserVoter::UPDATE_USER, $entity),
+                'delete'   => $this->isGranted(UserVoter::DELETE_USER, $entity),
+                'disable'  => $this->isGranted(UserVoter::DISABLE_USER, $entity),
+                'enable'   => $this->isGranted(UserVoter::ENABLE_USER, $entity),
+                'unlock'   => $this->isGranted(UserVoter::UNLOCK_USER, $entity),
+                'password' => $this->isGranted(UserVoter::SET_PASSWORD, $entity),
             ],
         ]);
     }
